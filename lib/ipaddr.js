whitequark/ipaddr.js

commit 500dca066f275fbaa00bf1cc1f0a29daa76a2ed5

Short-circuit IPv6 validity check with string.indexOf(":") == -1.
Fixes #27.

lib/ipaddr.js
@@ -369,8 +369,22 @@
     return this.parser(string) !== null;
   };
 
-  ipaddr.IPv4.isValid = ipaddr.IPv6.isValid = function(string) {
+  ipaddr.IPv4.isValid = function(string) {
     var e;
+    try {
+      new this(this.parser(string));
+      return true;
+    } catch (_error) {
+      e = _error;
+      return false;
+    }
+  };
+
+  ipaddr.IPv6.isValid = function(string) {
+    var e;
+    if (string.indexOf(":") === -1) {
+      return false;
+    }
     try {
       new this(this.parser(string));
       return true;

src/ipaddr.coffee
@@ -321,7 +321,19 @@ ipaddr.IPv4.isIPv4 = ipaddr.IPv6.isIPv6 = (string) ->
   return @parser(string) != null
 
 # Checks if a given string is a valid IPv4/IPv6 address.
-ipaddr.IPv4.isValid = ipaddr.IPv6.isValid = (string) ->
+ipaddr.IPv4.isValid = (string) ->
+  try
+    new this(@parser(string))
+    return true
+  catch e
+    return false
+
+ipaddr.IPv6.isValid = (string) ->
+  # Since IPv6.isValid is always called first, this shortcut
+  # provides a substantial performance gain.
+  if string.indexOf(":") == -1
+    return false
+
   try
     new this(@parser(string))
     return true

commit ef85d71d92fa1ed725c9c78c564c137d4b76619f

isValid() returns false for undefined string
Commit 500dca066f275fbaa00bf1cc1f0a29daa76a2ed5
introduced a regression for existing code that
passed an undefined string to isValid().

lib/ipaddr.js
@@ -382,7 +382,7 @@
 
   ipaddr.IPv6.isValid = function(string) {
     var e;
-    if (string.indexOf(":") === -1) {
+    if (!string || string.indexOf(":") === -1) {
       return false;
     }
     try {

src/ipaddr.coffee
@@ -331,7 +331,7 @@ ipaddr.IPv4.isValid = (string) ->
 ipaddr.IPv6.isValid = (string) ->
   # Since IPv6.isValid is always called first, this shortcut
   # provides a substantial performance gain.
-  if string.indexOf(":") == -1
+  if not string or string.indexOf(":") == -1
     return false
 
   try

test/ipaddr.test.coffee
@@ -154,6 +154,7 @@ module.exports =
     test.equal(ipaddr.IPv6.isValid('2001:db8::F53A::1'),    false)
     test.equal(ipaddr.IPv6.isValid('fe80::wtf'),            false)
     test.equal(ipaddr.IPv6.isValid('2002::2:'),             false)
+    test.equal(ipaddr.IPv6.isValid(undefined),              false)
     test.done()
 
   'parses IPv6 in different formats': (test) ->